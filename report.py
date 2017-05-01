0b445250c78343fbc1796335af71f3622678ad2e
Jacob Bieker <jacob@bieker.tech>
2017-05-01 23:02:48 +0200

    Updates for graphing
    Works, just can't get fitting to work properly with the given equation. Think I am missing something easy

--- report.py
+++ report.py
@@ -1,6 +1,7 @@
-import os, csv
+import os, csv, math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
 
 data = []
 data1 = []
@@ -20,8 +21,8 @@ inductor = 2.2 #milliHenry
 resistor = 150 #Ohm resistor
 
 files = list(os.walk("data"))
+file_count = 0
 for dataset in files[0][2]:
-    file_count = 0
     with open(os.path.join("data", dataset), "r") as csv_file:
         data.append([[],[],[]])
         data_no.append([[],[],[]])
@@ -40,5 +41,82 @@ for dataset in files[0][2]:
                 data_no[file_count][0].append([line[0]])
                 data_no[file_count][1].append([line[1]])
                 data_no[file_count][2].append([line[2]])
+    file_count += 1
+
+def get_c(L, w_nought):
+    return 1/(L*w_nought**2)
+
+def get_gamma(R):
+    return R/2
+
+def equation(R, Rm, L, w_nought, w):
+    # A is 150/?
+    # A2Γ2/(4(ω−ω0)2+Γ2/4)
+    top = ((Rm/R)**2)*((R/L)**2)/4
+    bottom = (w - w_nought)**2 + ((R/L)**2)/4
+    return top/bottom
+
+#  Create graphs
+def create_graph(frequency, vm, vlc, num_inductors):
+    w_nought = []
+    for element in frequency:
+        element = float(element[0])
+        w_nought.append(element/(2*np.pi))
+
+    vm_vlc = []
+    for index, element in enumerate(vm):
+        element = float(element[0])
+        bottom = float(vlc[index][0])
+        vm_vlc.append((element/bottom)**2)
+
+    def FWHM(X, Y):
+        d = []
+        for element in Y:
+            d.append(element - (max(Y) / 2))
+        d = np.asarray(d)
+        indexes = np.where(d > 0)[0]
+        return abs(X[indexes[-1]] - X[indexes[0]])
+
+    print(w_nought)
+    print("FWHM:" + str(FWHM(vm_vlc, w_nought)))
+    print(vm_vlc)
+
+    plt.scatter(w_nought,vm_vlc)
+
+create_graph(data[0][0], data[0][1], data[0][2], 1)
+create_graph(data_no[0][0], data_no[0][1], data_no[0][2], 1)
+plt.title("One Inductor")
+plt.ylabel("(Vm/Vlc)^2")
+plt.xlabel("w0")
+plt.show()
+
+create_graph(data[1][0], data[1][1], data[1][2], 2)
+create_graph(data_no[1][0], data_no[1][1], data_no[1][2], 2)
+plt.title("Two Inductors")
+plt.ylabel("(Vm/Vlc)^2")
+plt.xlabel("w0")
+plt.show()
+
+create_graph(data[2][0], data[2][1], data[2][2], 3)
+create_graph(data_no[2][0], data_no[2][1], data_no[2][2], 3)
+plt.title("Three Inductors")
+plt.ylabel("(Vm/Vlc)^2")
+plt.xlabel("w0")
+plt.show()
+
+create_graph(data[3][0], data[3][1], data[3][2], 4)
+create_graph(data_no[3][0], data_no[3][1], data_no[3][2], 4)
+plt.title("Four Inductors")
+plt.ylabel("(Vm/Vlc)^2")
+plt.xlabel("w0")
+plt.show()
+
+create_graph(data[4][0], data[4][1], data[4][2], 5)
+create_graph(data_no[4][0], data_no[4][1], data_no[4][2], 5)
+plt.title("Five Inductors")
+plt.ylabel("(Vm/Vlc)^2")
+plt.xlabel("w0")
+plt.show()
+
 
 
